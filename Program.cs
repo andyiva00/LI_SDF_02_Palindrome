c9c97eaf02a9561dac8d463c5b1040494d21cbc5
Andrey Ivanov <andyiva00@gmail.com>
2022-09-19 13:35:12 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// LinkedIN Learning Challenge
+// Learning Path: Software Development Fundamentals
+// Course: Learning C#
+// Challenge: Palindrome
+
+
+internal class Program
+{
+    private static (bool, int) CheckPalindrome(string inputString)
+    {
+        string tmpString = new string(inputString.Where(Char.IsLetter).ToArray()).ToLower();
+        bool isPalindrome = tmpString.Equals(new string (tmpString.ToCharArray().Reverse().ToArray()));
+
+        return (isPalindrome, isPalindrome ? inputString.Length : 0);
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Let's begin:");
+
+        while (true)
+        {
+            string? inputString = Console.ReadLine();
+
+            if (inputString == null || inputString.ToLower().Equals("exit"))
+            {
+                break;
+            }
+            else
+            {
+                (bool, int) result = CheckPalindrome(inputString);
+                Console.WriteLine($"Palindrome: {result.Item1}, Length: {result.Item2}");
+            }
+        }
+    }
+}
